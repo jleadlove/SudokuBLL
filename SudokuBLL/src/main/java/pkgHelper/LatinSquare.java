d6691c7606d4e19486c4e8707ad821261caa49cf
matth <matth@128.4.155.79>
2018-09-11 13:43:23 -0400

    Added ContainsZero() and doesElementExist

--- SudokuBLL/src/main/java/pkgHelper/LatinSquare.java
+++ SudokuBLL/src/main/java/pkgHelper/LatinSquare.java
@@ -11,6 +11,32 @@ public class LatinSquare {
 
 	}
 	
+	public boolean ContainsZero()
+	{
+		for(int iCol = 0; iCol < LatinSquare.length; iCol++ )
+		{
+			for(int iRow = 0; iRow <LatinSquare.length; iRow++)
+			{
+				if (LatinSquare[iCol][iRow]==0)
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+			
+	}
+	
+	public boolean doesElementExist(int[] arr, int iValue)
+	{
+		for(int i : arr)
+		{
+			if (i == iValue)
+				return true;
+		}
+		return false;
+	}
+	
 	public LatinSquare(int[] latinSquare) {
 		super();
 		LatinSquare = latinSquare;
